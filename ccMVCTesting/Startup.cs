8868e09ab36feb4b41fd3396986670ecd60513a1
jccamargo <camargodevelopment@gmail.com>
2015-03-14 12:00:02 -0600

    Initial Commit just basic MVC functionality with personal accounts

---
+++ ccMVCTesting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ccMVCTesting.Startup))]
+namespace ccMVCTesting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
